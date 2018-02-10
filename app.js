0228013e30fea664ca20af3bc4487aae3ea0bcc4
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2018-02-10 18:00:25 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
-const index = require('./routes/index');
-const campgrounds = require('./routes/campgrounds');
-const campgroundspost = require('./routes/campgroundspost');
-const newcampground = require('./routes/newcampground');
+const index = require('./routes/index'),
+    campgrounds = require('./routes/campgrounds'),
+    campgroundspost = require('./routes/campgroundspost'),
+    newcampground = require('./routes/newcampground');
 
 const app = express();
 
@@ -37,20 +38,20 @@ global.campgrounds = [
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
