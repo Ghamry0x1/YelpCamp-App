0894a18baa0aeea05853df0504f1f7dc46a0ad44
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2018-02-14 22:59:05 +0200

    deleted db connection file

--- models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yelp_camp', function(err) {
-    if (err) {
-        console.log('DB unable to connect to the server. Error:', err);
-    } else {
-        console.log('DB connected successfully');
-    }
-});
