56f4441b1e3ca0a931c8a3fce90917c1fccf7cf7
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2018-02-14 22:59:31 +0200

    added mongoose connection

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    db = require('./models/db');
+    mongoose = require('mongoose');
 
 global.Campground = require('./models/campground');
 
@@ -16,15 +16,22 @@ const index = require('./routes/index'),
 
 const app = express();
 
-// view engine setup
+mongoose.connect('mongodb://localhost/yelp_camp', function (err) {
+    if (err) {
+        console.log('DB unable to connect to the server. Error:', err);
+    }
+    else {
+        console.log('DB connected successfully');
+    }
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,14 +42,14 @@ app.use('/campgrounds/new', newcampground);
 app.use('/campgrounds', showcampground);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
